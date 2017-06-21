c0ccce890327ff492c5704cf7474db917a71a424
Munir Wanis <fallen.angel.pp@gmail.com>
2017-06-20 23:00:16 -0300

    Makes some crazy logic

--- Infnet.Aspnet.Assessment.Data/DataAuthors.cs
+++ Infnet.Aspnet.Assessment.Data/DataAuthors.cs
@@ -1,13 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using AuthorEntity = Infnet.Aspnet.Assessment.Entities.Author;
+using BookEntity = Infnet.Aspnet.Assessment.Entities.Book;
 
 namespace Infnet.Aspnet.Assessment.Data
 {
-    public class DataAuthors : IData<Author>
+    public class DataAuthors : IData<AuthorEntity>
     {
         public bool Delete(int id)
         {
@@ -15,45 +14,129 @@ namespace Infnet.Aspnet.Assessment.Data
             {
                 var author = db.Author.Find(id);
                 if (author == null) { return false; }
+                var authorBook = (from ab in db.AuthorBook where ab.AuthorId == id select ab).ToList();
+                authorBook.ForEach(ab => db.AuthorBook.Remove(ab));
                 db.Author.Remove(author);
                 return db.SaveChanges() > 0;
             }
         }
 
-        public Author Get(int id)
+        public AuthorEntity Get(int id)
         {
             using (var db = new LibraryDataModel())
             {
-                var author = db.Author.Find(id);
+                var author = (from a in db.Author
+                              join ab in db.AuthorBook on a.Id equals ab.AuthorId
+                              where a.Id == id
+                              select new AuthorEntity
+                              {
+                                  Birthdate = a.Birthdate,
+                                  Email = a.Email,
+                                  Id = a.Id,
+                                  LastName = a.LastName,
+                                  Name = a.Name,
+                                  Books = (from b in db.Books
+                                           where b.Id == ab.BookId
+                                           select new BookEntity
+                                           {
+                                               Id = b.Id,
+                                               Isbn = b.Isbn,
+                                               LauchDate = b.LauchDate,
+                                               Title = b.Title,
+                                               Authors = new List<AuthorEntity>()
+                                           }).ToList()
+                              }).SingleOrDefault();
                 return author;
             }
         }
 
-        public List<Author> GetAll()
+        public List<AuthorEntity> GetAll()
         {
             using (var db = new LibraryDataModel())
             {
-                return db.Author.ToList();
+                var authors = (from a in db.Author
+                               join ab in db.AuthorBook on a.Id equals ab.AuthorId
+                               select new AuthorEntity
+                               {
+                                   Birthdate = a.Birthdate,
+                                   Email = a.Email,
+                                   Id = a.Id,
+                                   LastName = a.LastName,
+                                   Name = a.Name,
+                                   Books = (from b in db.Books
+                                            where b.Id == ab.BookId
+                                            select new BookEntity
+                                            {
+                                                Id = b.Id,
+                                                Isbn = b.Isbn,
+                                                LauchDate = b.LauchDate,
+                                                Title = b.Title,
+                                                Authors = new List<AuthorEntity>()
+                                            }).ToList()
+                               }).ToList();
+                return authors;
             }
         }
 
-        public bool Insert(Author entry)
+        public bool Insert(AuthorEntity entry)
         {
+            var author = MapAuthor(entry);
             using (var db = new LibraryDataModel())
             {
-                db.Author.Add(entry);
+                db.Author.Add(author);
                 return db.SaveChanges() > 0;
             }
         }
 
-        public bool Update(int id, Author entry)
+        public bool Update(int id, AuthorEntity entry)
         {
+            var author = MapAuthor(entry);
             using (var db = new LibraryDataModel())
             {
-                db.Author.Attach(entry);
+                db.Author.Attach(author);
                 db.Entry(entry).State = EntityState.Modified;
                 return db.SaveChanges() > 0;
             }
+        }
+
+        private Author MapAuthor(AuthorEntity entry)
+        {
+            var authorBooks = new List<AuthorBook>();
+            foreach (var book in entry.Books)
+            {
+                var authorBook = new AuthorBook
+                {
+                    AuthorId = entry.Id,
+                    Author = new Author
+                    {
+                        Id = entry.Id,
+                        Birthdate = entry.Birthdate,
+                        Email = entry.Email,
+                        LastName = entry.LastName,
+                        Name = entry.Name
+                    },
+                    BookId = book.Id,
+                    Books = new Books
+                    {
+                        Id = book.Id,
+                        Isbn = book.Isbn,
+                        LauchDate = book.LauchDate,
+                        Title = book.Title
+                    }
+                };
+                authorBooks.Add(authorBook);
+            }
+            var author = new Author
+            {
+                Name = entry.Name,
+                LastName = entry.LastName,
+                Email = entry.Email,
+                Birthdate = entry.Birthdate,
+                Id = entry.Id,
+                AuthorBook = authorBooks
+            };
+
+            return author;
         }
     }
 }
